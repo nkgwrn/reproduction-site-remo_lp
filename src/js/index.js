c370dbf39e1a5683df81f3c80118f7d97e0885b0
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-05-19 12:45:36 +0900

    mvの実装

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,24 @@ window.addEventListener(
   },
   false
 );
+
+$(function () {
+  //背景画像のtopの位置を取得
+  var bg1_top = $(".p-mv").offset().top;
+
+  // ウィンドウの高さを取得
+  var win_h = $(window).height();
+
+  //背景画像を動かすタイミングの位置を計算
+  var start_bg1 = bg1_top - win_h;
+
+  $(window).scroll(function () {
+    //スクロール量
+    var y = $(this).scrollTop();
+
+    //スクロール量と動かすタイミング位置を判定した場合は背景画像を動かす
+    if (y >= start_bg1) {
+      $(".p-mv").css("background-position-y", -(y - bg1_top) * 0.3 + "px");
+    }
+  });
+});
